cbcec2c1be9f88eb8c18faf04a9e7decaffbf6d6
alanrichter53@gmail.com <alanrichter53@gmail.com>
2025-07-10 19:45:15 +0200

    Completed the game. Made a sections of snake that adds up after eating the food. Maybe I will add something more later.

--- Assets/Snake.cs
+++ Assets/Snake.cs
@@ -1,14 +1,19 @@
 using JetBrains.Annotations;
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
     private Vector2 direction = Vector2.right;
+    private List<Transform> segments = new List<Transform>();
+    public Transform segmentPre;
     LogicManager logic;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("LogicManager").GetComponent<LogicManager>();
+        segments.Add(this.gameObject.transform);
     }
 
     // Update is called once per frame
@@ -34,13 +39,29 @@ public class Snake : MonoBehaviour
 
     void FixedUpdate()
     {
+        for (int i = segments.Count - 1; i > 0; i--)
+        {
+            segments[i].position = segments[i - 1].position;
+        }
+
         this.transform.position = new Vector3(Mathf.Round(this.transform.position.x) + direction.x, Mathf.Round(this.transform.position.y)
             + direction.y, 0.0f);
     }
 
+    void Grow()
+    {
+        Transform segment = Instantiate(segmentPre);
+        segment.position = segments[segments.Count - 1].position;
+        segments.Add(segment);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Wall")
+        if (collision.gameObject.tag == "Food")
+        {
+            Grow();
+        }
+        else if (collision.gameObject.tag == "Wall")
         {
             logic.GameOver();
             this.gameObject.SetActive(false);
